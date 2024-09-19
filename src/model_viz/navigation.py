f4132e01f863dfd958ba6d41ef9382935a0dd1b4
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-19 18:50:42 +0200

    added function to obtain path set

---
+++ src/model_viz/navigation.py
@@ -0,0 +1,38 @@
+import os
+
+from model_viz.logging import get_logger
+
+logger = get_logger(__name__)
+
+
+def get_filepath_set(dirs: list[str] | None = None, files: list[str] | None = None) -> set[str]:
+    """Get the set of file paths to search for model classes."""
+    if (dirs is None) and (files is None):
+        raise ValueError("No directories or files provided.")
+
+    # declare set to store file paths
+    filepaths = set()
+    
+    # add files
+    if files is not None:
+        for file in files:
+            if not os.path.exists(file):
+                raise FileNotFoundError(f"File not found: {file}")
+            if file.endswith(".py"):
+                filepaths.add(file)
+            else:
+                logger.warning(f"File is not a python file: {file}")
+
+    # add directories
+    if dirs is not None:
+        for directory in dirs:
+            for root, _, filenames in os.walk(directory):
+                for filename in filenames:
+                    if filename.endswith(".py"):
+                        filepaths.add(os.path.join(root, filename))
+
+    # check if any files were found
+    if len(filepaths) == 0:
+        raise ValueError("No files found in provided dirs and files.")
+
+    return filepaths
