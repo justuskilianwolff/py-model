371f4f215eb2af7fcb6bb34690335b6836b20e41
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-19 18:51:19 +0200

    removed dotenv

--- src/model_viz/utils.py
+++ src/model_viz/utils.py
@@ -1,11 +1,6 @@
-from dotenv import load_dotenv
-
 from model_viz.logging import get_logger
 
 logger = get_logger(__name__)
-
-# load the environment variables
-load_dotenv(override=True)
 
 
 def vulture_ignore(obj):
