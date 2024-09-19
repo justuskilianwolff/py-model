eb5c637ba404c3042169145c6f62d1e26872e9da
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-19 23:31:03 +0200

    testing

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from model_viz.class_instance import ClassInstance
 from model_viz.logging import get_logger
 from model_viz.navigation import get_classes, get_filepath_set
 from model_viz.parser import parser
+from model_viz.string_repr import get_attr_repr
 
 args = parser.parse_args()
 
-logger = get_logger(__name__, level="WARNING")
+logger = get_logger(__name__, level="ERROR")
 
 # convert args to a dictionary
 args_dict = vars(args)
 
 # for testing purposes add dir
-args_dict["dirs"] = ["examp5le_models/model_set_1"]
+args_dict["dirs"] = ["example_models/model_set_1"]
 
 # get the file paths
 filepaths = get_filepath_set(dirs=args_dict.get("dirs"), files=args_dict.get("files"))
@@ -32,3 +33,15 @@ for filepath in sorted(filepaths):
 # convert to list
 for class_instance in class_instances:
     print(class_instance)
+
+joined_attributes = []
+class_names = []
+for cls in class_instances:
+    for attr in cls.attributes:
+        joined_attributes.append(attr)
+        class_names.append(cls.class_name)
+
+print(joined_attributes)
+
+for cls_name, attr in zip(class_names, joined_attributes):
+    print(cls_name + " " + get_attr_repr(attr))
