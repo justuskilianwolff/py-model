b5b2ac37d6ec55ef99e07819d1db6e4c82fec7db
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-19 22:43:44 +0200

    indicate access level

--- src/model_viz/utils.py
+++ src/model_viz/utils.py
@@ -6,3 +6,17 @@ logger = get_logger(__name__)
 def vulture_ignore(obj):
     """Decorator to ignore vulture warnings for a function."""
     return obj
+
+
+def indicate_access_level(name: str) -> str:
+    """Indicate access level of a class, function or attribute."""
+
+    if name.startswith("__"):
+        # double underscore indicates private
+        return "- " + name[2:]
+    elif name.startswith("_"):
+        # single underscore indicates protected
+        return "# " + name[1:]
+    else:
+        # no underscore indicates public
+        return "+ " + name
