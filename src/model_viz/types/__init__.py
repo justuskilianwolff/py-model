5d7f613e21ac6418e1fe1344f65abb984f8a7790
justuskilianwolff <justus.wolff@web.de>
2024-09-20 11:17:14 +0200

    changed import order

--- src/model_viz/types/__init__.py
+++ src/model_viz/types/__init__.py
@@ -1,6 +1,6 @@
+from .typehintablevalue import TypeHintableValue  # noqa: I001 # needs to be first to avoid circular import
 from .attribute import Attribute
 from .function import Function
 from .parameter import Parameter
-from .typehintablevalue import TypeHintableValue
 
 __all__ = ["Attribute", "Function", "Parameter", "TypeHintableValue"]
