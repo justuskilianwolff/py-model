e816b46fd61a568335ff2902f0028c10cd5e1b0e
Justus <justus.wolff@audit.vision>
2024-09-20 08:45:37 +0200

    added handle function

--- src/model_viz/utils.py
+++ src/model_viz/utils.py
@@ -1,4 +1,8 @@
+import ast
+
 from model_viz.logging import get_logger
+
+from .errors import NotImplementedError
 
 logger = get_logger(__name__)
 
@@ -20,3 +24,16 @@ def indicate_access_level(name: str) -> str:
     else:
         # no underscore indicates public
         return "+" + name
+
+
+def handle_function_arg(arg: ast.arg) -> tuple[str, str | None]:
+    """Handle function argument."""
+
+    if isinstance(arg.annotation, ast.Name):
+        tp = arg.annotation.id
+    elif arg.annotation is None:
+        tp = None
+    else:
+        raise NotImplementedError()
+    
+    return arg.arg, tp
