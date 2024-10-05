b902a6320cf9fc6ef1901c40b12a2458a7447ca3
Justus Kilian Wolff <justus.wolff@web.de>
2024-10-05 13:17:35 +0200

    updated help

--- src/py_model/parser.py
+++ src/py_model/parser.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser(description="Argument parser for for the py-mod
 parser.add_argument("--dirs", "-d", nargs="?", type=str, help="Directories to search for model files.")
 parser.add_argument("--files", "-f", nargs="?", type=str, help="Files to search for model files.")
 parser.add_argument(
-    "--output", "-o", type=str, default="py-model.dot", help="Output file to save the visualization file."
+    "--output", "-o", type=str, default="py-model.dot", help="Output path of the result"
 )
 parser.add_argument(
     "--verbose", "-v", action="store_true", help="Increase verbosity of the output."
